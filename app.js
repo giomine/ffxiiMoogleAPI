401e26f59da3af08257c6d0807657f5e80cc83c7
giomine <giomineo@gmail.com>
2021-02-27 17:08:22 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+fetch('https://www.moogleapi.com/api/v1/characters/search?origin=xii')
+.then(res => res.json())
+.then(data => {
+    data.forEach((char) => {
+    console.log(char.pictures[0].url + " Name: " + char.name + ", Origin: " + char.origin + ", About: " + char.description)  
+
+    const card = document.createElement('div')
+    card.setAttribute('class', 'card')
+
+    const image = document.createElement('img');
+    image.src = char.pictures[0].url
+
+    const name = document.createElement('h2');
+    name.textContent = char.name;
+
+    const origin = document.createElement('p');
+    origin.textContent = char.origin;
+
+    const description = document.createElement('p');
+    description.textContent = char.description;
+
+    container.appendChild(card)
+    card.appendChild(image)
+    card.appendChild(name)
+    card.appendChild(origin)
+    card.appendChild(description)
+})
+})
+    .catch(error => {
+        console.log(error)
+})
+
+
+const app = document.getElementById('root')
+
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+
+app.appendChild(container)
